de6eadf583515a3c15a6e916cb7628aba2b5f62c
Tomomi Kajita <kajip@icloud.com>
2024-06-21 16:16:07 +0900

    Testワークフローを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
+import stylisticJs from '@stylistic/eslint-plugin-js'
 import stylisticTs from '@stylistic/eslint-plugin-ts'
 
 
@@ -12,14 +13,18 @@ export default [
     ...tseslint.configs.stylistic,
     {
         plugins: {
-            '@stylistic/ts': stylisticTs
+          '@stylistic/js': stylisticJs,
+          '@stylistic/ts': stylisticTs,
         },
         rules: {
-            'no-unused-vars': 'off',
-            '@typescript-eslint/no-unused-vars': 'error',
-            // indent
-            "@stylistic/js/indent": ["error", 4],
-            "@stylistic/ts/indent": ["error", 4],
+          'no-unused-vars': 'off',
+          '@typescript-eslint/no-unused-vars': 'error',
+          // indent
+          "@stylistic/js/indent": ["error", 4],
+          "@stylistic/ts/indent": ["error", 4],
+          // 行末のセミコロンをつけない
+          "@stylistic/js/semi": ["error", "never"],
+          "@stylistic/ts/semi": ["error", "never"],
         }
     }
 ]
